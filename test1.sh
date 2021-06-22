423829ecefa8e2aed60eded142c148d481c969fe
thevoice259 <86289505+thevoice259@users.noreply.github.com>
2021-06-22 02:18:23 -0700

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo docker run -itd -p 80:6080 -e PASSWORD=PassworD1982 -e SUDO=yes -e NGROK=yes chenjr0719/ubuntu-mate-novnc
 sudo docker run -itd -p 8080:6080 -e PASSWORD=PassworD1982 -e SUDO=yes -e NGROK=yes chenjr0719/ubuntu-mate-novnc
-done
